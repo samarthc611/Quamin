4bc1ab1d6c811cc22a8fc2b494bbd4024916c23c
Samarth_Chitnis <220010052@iitdh.ac.in>
2024-07-11 16:56:48 +0530

    commit_5

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 from bson import ObjectId 
+import os
 
 app = Flask(__name__)
 
@@ -36,4 +37,5 @@ def get_documents():
     return jsonify(documents)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
