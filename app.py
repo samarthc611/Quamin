84d142ed2ecef48def58211f25424259792b0b40
Samarth_Chitnis <220010052@iitdh.ac.in>
2024-07-11 16:38:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from bson import ObjectId 
+
+app = Flask(__name__)
+
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.certificates  
+collection = db.all_certificates  
+
+
+@app.route('/verify', methods=['GET'])
+def get_info_based_on_id():
+
+    cert_id = request.args.get('cert_id')
+
+    try:
+        obj_id = ObjectId(cert_id)
+    except Exception as e:
+        return jsonify({"error": "Invalid ObjectId format"}), 400
+
+    document = collection.find_one({"_id": obj_id})
+
+    if document:
+        document['_id'] = str(document['_id'])
+        return render_template('verify.html', cert_id=document['_id'], name=document['name'])
+    else:
+        return jsonify({"error": "Document not found"}), 404
+
+@app.route('/documents', methods=['GET'])
+def get_documents():
+    documents = list(collection.find())
+    for doc in documents:
+        doc['_id'] = str(doc['_id']) 
+    return jsonify(documents)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ generator.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from pymongo import MongoClient
+import os
+import qrcode
+
+app = Flask(__name__)
+
+
+client = MongoClient("mongodb+srv://220010052:KC09AxTI9vP555Vh@cluster0.fmo3c2z.mongodb.net/")
+db = client.certificates  
+collection = db.all_certificates  
+
+base_url = "http://127.0.0.1:5000/verify?cert_id="
+
+documents = collection.find()
+for doc in documents:
+    cert_id = str(doc['_id'])
+
+    verification_url = base_url + cert_id
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(verification_url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+
+    file_path = os.path.join(os.getcwd(), f"certificate_qr_code_{cert_id}.png")
+    img.save(file_path)
+
+    print(f"QR code generated and saved as {file_path}")
